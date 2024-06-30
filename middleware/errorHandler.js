f209873d05ba0953559225a85b835413e9d59b36
safadmt <safadmt53@outlook.com>
2024-06-30 11:25:58 +0530

    update errorhandling

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,12 @@
 import logger from "../utils/logger.js";
 function handleError(err, req, res, next) {
-  logger.error(err.message ,err.stack)
+  
   res.render("partials/message/pagenotfound", {
     status: 500,
     error: "Something went. wrong please try again later",
     role : "error"
   });
+  logger.info(err.stack || err.message)
   next();
 }
 
